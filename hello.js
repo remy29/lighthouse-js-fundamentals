8d673b56e26e99307d02482ab5738a541758403a
Arthur Remy <arthur.d.remy@gmail.com>
2020-09-16 21:10:44 +0000

    learning to write functions

--- hello.js
+++ hello.js
@@ -8,9 +8,18 @@ sayHello();
 */
 
 //Example 2
-
+/*
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
 console.log(sayHello('Arthur'));
+*/
+
+//Example 3
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
