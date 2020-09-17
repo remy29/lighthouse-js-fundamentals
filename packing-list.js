bf215f3f25a0c8a082c3043ef4f07e1792c10098
Arthur Remy <arthur.d.remy@gmail.com>
2020-09-17 20:26:36 +0000

    packing list

---
+++ packing-list.js
@@ -0,0 +1,13 @@
+//Example 1
+/*
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+*/
+
+//Example 2
+
+
